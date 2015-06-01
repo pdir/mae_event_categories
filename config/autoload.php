6f6257a47b9cec6249cdf08f551e559e8ab63a73
Sven Baumann <baumann.sv@gmail.com>
2015-06-01 08:55:13 +0200

    add the model to the project

--- config/autoload.php
+++ config/autoload.php
@@ -3,5 +3,8 @@ ClassLoader::addNamespace('MaeEventCategories');
 
 ClassLoader::addClasses(array
 (
-    'MaeEventCategories\MaeEvent' => 'system/modules/mae_event_categories/classes/MaeEvent.php'
+    'MaeEventCategories\MaeEvent' => 'system/modules/mae_event_categories/classes/MaeEvent.php',
+
+    // Models
+	'MaeEventCategories\MaeEventCatModel' => 'system/modules/mae_event_categories/model/MaeEventCatModel.php'
 ));
