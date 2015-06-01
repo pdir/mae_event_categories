6fa23873d074e80e7b0f047e539840173f4fa80b
Sven Baumann <baumann.sv@gmail.com>
2015-06-01 17:36:11 +0200

    fixed autoload.php and directory name for models

--- config/autoload.php
+++ config/autoload.php
@@ -1,7 +1,10 @@
 <?php
-ClassLoader::addNamespace('MaeEventCategories');
+ClassLoader::addNamespace(array('MaeEventCategories'));
 
 ClassLoader::addClasses(array
 (
-    'MaeEventCategories\MaeEvent' => 'system/modules/mae_event_categories/classes/MaeEvent.php'
+    'MaeEventCategories\MaeEvent' => 'system/modules/mae_event_categories/classes/MaeEvent.php',
+
+    // Models
+	'MaeEventCategories\MaeEventCatModel' => 'system/modules/mae_event_categories/models/MaeEventCatModel.php'
 ));
