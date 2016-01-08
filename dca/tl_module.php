9238e875f97b920f24e847749dafd6ea5dd30a98
Martin <kontakt@martin-eberhardt.com>
2016-01-08 10:32:09 +0100

    added event categories selection to calendar module

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,6 +1,7 @@
 <?php
 // new categories blob field for use in standard eventlist module
-$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist']   = str_replace(';{protected_legend:hide}', ';{event_cat_legend:hide},event_categories;{protected_legend:hide}', $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist']);
+$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist']    = str_replace(';{protected_legend:hide}', ';{event_cat_legend:hide},event_categories;{protected_legend:hide}', $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist']);
+$GLOBALS['TL_DCA']['tl_module']['palettes']['calendar']     = str_replace(';{protected_legend:hide}', ';{event_cat_legend:hide},event_categories;{protected_legend:hide}', $GLOBALS['TL_DCA']['tl_module']['palettes']['calendar']);
 $GLOBALS['TL_DCA']['tl_module']['fields']['event_categories'] = array
 (
     'label'                   => &$GLOBALS['TL_LANG']['tl_module']['event_categories'],
